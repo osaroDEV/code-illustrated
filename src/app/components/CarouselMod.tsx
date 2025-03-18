b07606fdc76ee1c339406706148adad68f3e1b00
osarodev <osernon@gmail.com>
2025-03-18 14:46:43 +0100

    make carousel responsive

--- src/app/components/CarouselMod.tsx
+++ src/app/components/CarouselMod.tsx
@@ -2,7 +2,6 @@
 import * as React from "react"
 import Autoplay from "embla-carousel-autoplay"
 import Image from 'next/image'
-
 import { Card, CardContent } from "@/components/ui/card"
 import {
   Carousel,
@@ -16,38 +15,39 @@ export function CarouselMod() {
   const plugin = React.useRef(
     Autoplay({ delay: 2000, stopOnInteraction: true })
   )
-
+  
   const images = [
     "/images/flying.jpg",
     "/images/looking.jpg",
     "/images/smiling.jpg",
     "/images/shaking.jpg",
   ]
-
+  
   return (
     <Carousel
       plugins={[plugin.current]}
-      className="w-full max-w-xs"
+      className="w-full"
       onMouseEnter={plugin.current.stop}
       onMouseLeave={plugin.current.reset}
     >
       <CarouselContent>
         {images.map((src, index) => (
-          <CarouselItem key={index}>
-            <div className="w-full h-[350px] border border-red-500">
+          <CarouselItem key={index} className="w-full">
+            <div className="relative w-full h-[350px] md:h-[500px]">
               <Image
                 src={src}
                 alt={`Image ${index + 1}`}
-                layout="fill"
-                objectFit="cover"
-                className="w-full h-full"
+                fill
+                sizes="100vw"
+                priority={index === 0} 
+                className="object-cover"
               />
             </div>
           </CarouselItem>
         ))}
       </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
+      <CarouselPrevious className="left-2" />
+      <CarouselNext className="right-2" />
     </Carousel>
   )
 }
