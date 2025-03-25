d7532cb7645286ad65eaaca05c06baf77df472c7
osarodev <osernon@gmail.com>
2025-03-25 16:13:32 +0100

    add background to Services

--- src/app/components/WhatsIncluded.tsx
+++ src/app/components/WhatsIncluded.tsx
@@ -3,7 +3,7 @@ import { services } from '../../lib/data/services'
 
 const WhatsIncluded = () => {
   return (
-    <section className="bg-[#f9fafb] flex flex-wrap items-center justify-center gap-4 px-4 py-8 md:px-8 md:py-12 xl:px-12 xl:py-16 xxl:px-16 xxl:py-20">
+    <section className="bg-[#00668f] bg-[url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI1NiIgaGVpZ2h0PSIxMDAiPgo8cmVjdCB3aWR0aD0iNTYiIGhlaWdodD0iMTAwIiBmaWxsPSJyZ2JhKDAsMCwwLDApIj48L3JlY3Q+CjxwYXRoIGQ9Ik0yOCA2NkwwIDUwTDAgMTZMMjggMEw1NiAxNkw1NiA1MEwyOCA2NkwyOCAxMDAiIGZpbGw9Im5vbmUiIHN0cm9rZT0icmdiYSgwLCA5OCwgMTM3LCAxLjApIiBzdHJva2Utd2lkdGg9IjIiPjwvcGF0aD4KPHBhdGggZD0iTTI4IDBMMjggMzRMMCA1MEwwIDg0TDI4IDEwMEw1NiA4NEw1NiA1MEwyOCAzNCIgZmlsbD0ibm9uZSIgc3Ryb2tlPSJyZ2JhKDAsIDk4LCAxMzcsIDEuMCkiIHN0cm9rZS13aWR0aD0iMiI+PC9wYXRoPgo8L3N2Zz4=')] bg-repeat flex flex-wrap items-center justify-center gap-4 px-4 py-8 md:px-8 md:py-12 xl:px-12 xl:py-16 xxl:px-16 xxl:py-20">
       <>
         {services.map((service) => (
           <div
